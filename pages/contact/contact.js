260ab4f8e149aabe6128b20d7e3db8a900f842ec
Funminiyi Adedoyin <doyinfummi2012@gmail.com>
2022-03-22 13:01:36 +0100

    Home page Reset

--- pages/contact/contact.js
+++ pages/contact/contact.js
@@ -10,3 +10,12 @@ document.getElementById('messageForm').addEventListener('submit', () => {
     sessionStorage.setItem(`${name}'s Message`, JSON.stringify(userInput));
 });
 // ****END CONTACT.HTML******
+
+let toggle = document.getElementById("toggle")
+
+let links = document.getElementById("nav-links")
+console.log(links);
+
+let displayNavLinks = () => {
+    links.classList.toggle("show")
+}
