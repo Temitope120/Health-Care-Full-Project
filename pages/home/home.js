183c2d9e46ccf07285f6ad450a75475e78569fa0
Funminiyi Adedoyin <doyinfummi2012@gmail.com>
2022-03-23 15:04:08 +0100

    Responsiveness of pages

---
+++ pages/home/home.js
@@ -0,0 +1,10 @@
+let toggleIcon = document.getElementById("toggle-icon")
+console.log(toggleIcon);
+
+let toggleMenu = document.getElementById("toggle-menu")
+
+let displayNavLinks = () => {
+
+    toggleMenu.classList.toggle("show")
+} 
+displayNavLinks()
